56e754bc29a08a78130b1aa2271cc17daa1a9b46
Dawid Czarnecki <85833530+dawidekkk@users.noreply.github.com>
2022-04-21 20:37:16 +0200

    test deploy

--- deploy.bat
+++ deploy.bat
@@ -20,7 +20,7 @@ git commit -m 'deploy'
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git main
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
-git push -f git@github#.com:dawidekkk/seven-more-vue-deploy.git master:gh-pages
+git push -f git@github#.com:dawidekkk/sevenmorevue.git master:gh-pages
 
 # git push -f git@github.com:dawidekkk/seven-more-vue.git master:gh-pages
 cd -
