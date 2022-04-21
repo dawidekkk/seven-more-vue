d142b1745763ce1f02008009cede30c0dfad7bf5
Dawid Czarnecki <85833530+dawidekkk@users.noreply.github.com>
2022-04-21 14:43:42 +0200

    deploy prepare

--- vue.config.js
+++ vue.config.js
@@ -1,8 +1,9 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
+  publicPath: '/seven-more-vue'
 })
 
-module.exports = {
-  publicPath: process.env.NODE_ENV === "production" ? "/https://dawidekkk.github.io/seven-more-vue/" : "/",
-};
+// module.exports = {
+  
+// };
