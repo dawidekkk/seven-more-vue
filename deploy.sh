8b3262457906e7569ab3389f6b3239d3b8e0d9e9
Dawid Czarnecki <85833530+dawidekkk@users.noreply.github.com>
2022-04-21 17:20:20 +0200

    deploy.sh comma added

--- deploy.sh
+++ deploy.sh
@@ -22,5 +22,5 @@ git commit -m 'deploy'
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
 # git push -f git@github.com:<USERNAME>/<REPO>.git main:gh-pages
 
-git push -f git@github.com:dawidekkk/seven-more-vue-git master:gh-pages
+git push -f git@github.com:dawidekkk/seven-more-vue.git master:gh-pages
 cd -
