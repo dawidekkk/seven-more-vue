3d195ffd1d42b72e177fc8044216b61d7f4c89f4
Dawid Czarnecki <85833530+dawidekkk@users.noreply.github.com>
2022-04-21 20:26:48 +0200

    test deploy

--- vue.config.js
+++ vue.config.js
@@ -5,7 +5,7 @@ module.exports = defineConfig({
 })
 
 module.exports = {
-publicPath: process.env.NODE_ENV === 'production'
-  ? '/seven-more-vue/'
-  : '/'
-};
+  publicPath: process.env.NODE_ENV === 'production'
+    ? '/'
+    : '/'
+}
