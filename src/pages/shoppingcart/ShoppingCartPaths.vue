0abd1b8f14aab73491ad8f2a76d8d85ba8d31f38
Dawid Czarnecki <85833530+dawidekkk@users.noreply.github.com>
2022-05-17 20:35:32 +0200

    all colorful route paths

--- src/pages/shoppingcart/ShoppingCartPaths.vue
+++ src/pages/shoppingcart/ShoppingCartPaths.vue
@@ -1,18 +1,18 @@
 <template>
   <div class="paths">
-    <div class="path" :class="{ activeShop }">
+    <div class="path" :class="{ activeShop: loginPath('koszyk') }">
       <p>1. Koszyk</p>
     </div>
-    <div class="path" :class="{activeShop: this.$route.path === '/logowanie'}">
+    <div class="path" :class="{ activeShop: loginPath('logowanie') }">
       <p>2. Logowanie</p>
     </div>
-    <div class="path">
+    <div class="path" :class="{ activeShop: loginPath('transport') }">
       <p>3. Transport</p>
     </div>
-    <div class="path">
+    <div class="path" :class="{ activeShop: loginPath('platnosc') }">
       <p>4. Płatność</p>
     </div>
-    <div class="path">
+    <div class="path" :class="{ activeShop: loginPath('zakonczenie') }">
       <p>5. Zakończenie</p>
     </div>
   </div>
@@ -25,7 +25,13 @@ export default {
     activeShop: String,
   },
 
-
+  methods: {
+    loginPath(path) {
+      if(this.$route.path === `/${path}`) {
+        return 'activeShop';
+      }
+    }
+  }
 
 };
 </script>
